8a2aac1ab6422e67cd10a27b27e5b1fb3ab8552a
Gordon <gordonzhou68@outlook.com>
2019-05-06 23:01:59 +0000

    everything commited

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,20 @@
 var request = require('request');
+var secrets = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent': 'request',
+          'Authorization': `token ${secrets.GITHUB_TOKEN}`
+        }
+      };
+
+    request(options, function(err, res, body){
+        cb(err,body);
+    });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
