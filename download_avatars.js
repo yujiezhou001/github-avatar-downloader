1b501b4ae9ed0c2d04d55188816ebedb65b54233
Gordon <gordonzhou68@outlook.com>
2019-05-07 00:16:02 +0000

    Support Command line Arguments exercese completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,5 @@
+var repoowner = process.argv[2];
+var reponame = process.argv[3]
 var request = require('request');
 var secrets = require('./secrets')
 var fs = require('fs');
@@ -18,14 +20,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
     });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(repoowner, reponame, function(err, result) {
     console.log("Errors:", err);
-    result.forEach(function(element){
-        var imageurl = element.avatar_url;
-        var filepath = "./images/" + element.login +".jpg"
-        downloadImageByURL(imageurl, filepath)
-    })
-  });
+    if (repoowner && reponame) {
+        result.forEach(function(element){
+            var imageurl = element.avatar_url;
+            var filepath = "./images/" + element.login +".jpg"
+            downloadImageByURL(imageurl, filepath)
+        })
+    } else {
+        console.log("You didn't specify both arguments!")
+    }
+})
 
 
 function downloadImageByURL(url, filePath) {
