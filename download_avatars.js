e845a00bc1bb8119e592eb117931a122c5b9668a
Gordon <gordonzhou68@outlook.com>
2019-05-07 00:37:12 +0000

    Comments Added

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,15 @@
+//Define repository owner and name to pass them directly on commandline.
+//require all the necessary packages.
+
 var repoowner = process.argv[2];
-var reponame = process.argv[3]
+var reponame = process.argv[3];
 var request = require('request');
-var secrets = require('./secrets')
+var secrets = require('./secrets');
 var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//Request function 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -16,24 +20,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
       };
 
     request(options, function(err, res, body){
+        //convert strings to object in order to extract useful information in execution.
         cb(err,JSON.parse(body));
     });
 }
-
+//Execution of Request Function & stores urls and filepaths
 getRepoContributors(repoowner, reponame, function(err, result) {
     console.log("Errors:", err);
     if (repoowner && reponame) {
         result.forEach(function(element){
             var imageurl = element.avatar_url;
-            var filepath = "./images/" + element.login +".jpg"
+            var filepath = "./images/" + element.login +".jpg";
             downloadImageByURL(imageurl, filepath)
-        })
+        });
     } else {
-        console.log("You didn't specify both arguments!")
+        console.log("You didn't specify both arguments!");
     }
 })
 
-
+//Another request function that passes its input content to output file.
 function downloadImageByURL(url, filePath) {
     request.get(url)           
        .on('error', function (err) {  
@@ -41,8 +46,8 @@ function downloadImageByURL(url, filePath) {
          console.log('error!'); 
          }                 
        })
-       .on('response', function (response) {                           
-       })
+    // .on('response', function (response) {                           
+    // })
        .pipe(fs.createWriteStream(filePath));               
 }
 
